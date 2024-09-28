bc3e98c8f99bcc30daf9de56249131fb5921fce5
shoufu <huge20901@gmail.com>
2024-09-29 07:45:12 +0800

    Update shoufu_auto_run.sh

--- shoufu_auto_run.sh
+++ shoufu_auto_run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# 定义颜色变量
 purple="\033[35m"
 reset="\033[0m"  # 用于重置颜色
 
+# 输出欢迎信息
 echo -e "${purple}---------------------------------"
 echo -e "|  ----首----     ----富----   |"
 echo -e "| |         |   |          |  |"
@@ -19,12 +21,6 @@ echo "加入首富电报财富群: https://t.me/shoufuhuge 一起暴富"
 echo "首富x推特: https://x.com/huge2090"
 echo "========================================================================"
 
-# 检查是否以root用户运行
-if [[ $EUID -ne 0 ]]; then
-   echo "请以root用户运行此脚本!" 
-   exit 1
-fi
-
 # 显示主菜单
 while true; do
     echo "========================================================================" 
@@ -35,11 +31,12 @@ while true; do
 
     case $OPTION in 
         1)  
+            # 安装 ALEO 一键挖矿脚本
             cd ~
-            rm -rf shoufu_f2pool_aleominer.sh
-            wget https://raw.githubusercontent.com/ShoufuMining/aleo-windows-f2pool/main/shoufu_f2pool_aleominer.sh
+            rm -f shoufu_f2pool_aleominer.sh  # 使用 -f 防止错误
+            wget -q https://raw.githubusercontent.com/ShoufuMining/aleo-windows-f2pool/main/shoufu_f2pool_aleominer.sh || { echo "下载失败"; exit 1; }
             chmod u+x shoufu_f2pool_aleominer.sh
-            ./shoufu_f2pool_aleominer.sh
+            ./shoufu_f2pool_aleominer.sh || { echo "脚本执行失败"; exit 1; }
             ;; 
         2) 
             echo "待更新..." 
@@ -54,3 +51,4 @@ while true; do
             ;; 
     esac 
 done
+
