00c8496c54838be3712ba237f48057600bdf6e3f
shoufu <huge20901@gmail.com>
2024-09-29 06:36:31 +0800

    Update shoufu_auto_run.sh

--- shoufu_auto_run.sh
+++ shoufu_auto_run.sh
@@ -10,7 +10,7 @@ echo -e "| |     |   |   |     |    |  |"
 echo -e "|  ----首----     ----富----   |"
 echo -e "---------------------------------\033[0m"
 
-# 定义可选的下载链接
+# 使用有效的下载链接
 ALEO_DOWNLOAD_URL="https://gh-proxy.com/https://raw.githubusercontent.com/ShoufuMining/aleo-windows-f2pool/main/shoufu_f2pool_aleominer.sh"
 
 # 创建菜单，让用户选择要执行的项目
@@ -25,27 +25,24 @@ read -p "请输入选项编号 (1, 2, q): " option
 case $option in
   1)
     echo "您选择了安装 ALEO 一键挖矿脚本。"
-    DOWNLOAD_URL="$ALEO_DOWNLOAD_URL"
     echo "正在下载并执行 ALEO 挖矿脚本..."
 
     # 在执行下载前打印出当前的链接，确保正确性
-    echo "当前下载链接: $DOWNLOAD_URL"
+    echo "当前下载链接: $ALEO_DOWNLOAD_URL"
 
-    # 检查是否为空链接
-    if [[ -z "$DOWNLOAD_URL" ]]; then
-      echo "下载链接为空，请检查下载链接的设置。"
-      exit 1
-    fi
-
-    wget -qO- "$DOWNLOAD_URL" | bash
+    # 下载并执行挖矿脚本
+    wget -qO- "$ALEO_DOWNLOAD_URL" | bash
     ;;
+
   2)
     echo "该选项尚未更新，敬请期待！"
     ;;
+  
   q)
     echo "已退出脚本。"
     exit 0
     ;;
+  
   *)
     echo "无效的选项，请重新运行脚本并选择正确的选项。"
     ;;
