80df7844d473659444fab4764aa27276af54094d
shoufu <huge20901@gmail.com>
2024-09-29 07:19:15 +0800

    Update shoufu_auto_run.sh

--- shoufu_auto_run.sh
+++ shoufu_auto_run.sh
@@ -1,44 +1,56 @@
 #!/bin/bash
 
-# 显示欢迎信息
-echo -e "\033[35m---------------------------------"
+purple="\033[35m"
+reset="\033[0m"  # 用于重置颜色
+
+echo -e "${purple}---------------------------------"
 echo -e "|  ----首----     ----富----   |"
 echo -e "| |         |   |          |  |"
 echo -e "| |  ----   |   |---- ---- |  |"
 echo -e "| |     |   |   |     |    |  |"
 echo -e "| |     |   |   |     |    |  |"
 echo -e "|  ----首----     ----富----   |"
-echo -e "---------------------------------\033[0m"
+echo -e "${purple}---------------------------------${reset}"
 
-# 定义可选的下载链接
-ALEO_DOWNLOAD_URL="https://gh-proxy.com/https://raw.githubusercontent.com/ShoufuMining/aleo-windows-f2pool/main/shoufu_f2pool_aleominer.sh"
+echo -e "${purple}======================首富虎哥一键挖矿脚本===================" 
+echo "首富频道出品，仅供会员学习使用，请勿用于商业用途"
+echo "首富Youtube频道: https://www.youtube.com/@shoufu"
+echo "加入首富电报财富群: https://t.me/shoufuhuge 一起暴富"
+echo "首富x推特: https://x.com/huge2090"
+echo "========================================================================"
 
-# 创建菜单，让用户选择要执行的项目
-echo -e "\033[32m请选择操作选项: \033[0m"
-echo "1) 下载并运行 ALEO 一键挖矿脚本"
-echo "q) 退出"
+# 检查是否以root用户运行
+if [[ $EUID -ne 0 ]]; then
+   echo "请以root用户运行此脚本!" 
+   exit 1
+fi
 
-read -p "请输入选项编号 (1, q): " option
+# 显示主菜单
+while true; do
+    echo "========================================================================" 
+    echo "1) 安装 ALEO 一键挖矿脚本" 
+    echo "2) 待更新..." 
+    echo "q) 退出" 
+    read -p "请输入选项: " OPTION 
 
-# 检查用户输入的选项并执行相应操作
-case $option in
-  1)
-    echo "您选择了下载并运行 ALEO 一键挖矿脚本。"
-    
-    # 下载脚本
-    wget -O shoufu_f2pool_aleominer.sh "$ALEO_DOWNLOAD_URL"
-    
-    # 赋予执行权限
-    chmod +x shoufu_f2pool_aleominer.sh
-    
-    # 运行下载的脚本
-    ./shoufu_f2pool_aleominer.sh
-    ;;
-  q)
-    echo "已退出脚本。"
-    exit 0
-    ;;
-  *)
-    echo "无效的选项，请重新运行脚本并选择正确的选项。"
-    ;;
-esac
+    case $OPTION in 
+        1)  
+            cd ~
+            rm -rf shoufu_f2pool_aleominer.sh
+            wget https://raw.githubusercontent.com/ShoufuMining/aleo-windows-f2pool/main/shoufu_f2pool_aleominer.sh
+            chmod u+x shoufu_f2pool_aleominer.sh
+            ./shoufu_f2pool_aleominer.sh
+            ;; 
+        2) 
+            echo "待更新..." 
+            ;; 
+        q) 
+            echo "退出程序。" 
+            exit 0 
+            ;; 
+        *) 
+            echo "无效选项，请重新输入。" 
+            sleep 1 
+            ;; 
+    esac 
+done
