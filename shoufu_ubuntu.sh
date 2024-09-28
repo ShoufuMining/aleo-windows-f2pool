b75a97efd9737c7466ad92add3f27e8c899a45bc
shoufu <huge20901@gmail.com>
2024-09-29 05:49:07 +0800

    Delete shoufu_ubuntu.sh

--- shoufu_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 一键下载并运行 Aleo 挖矿脚本
-echo "正在下载 Aleo 挖矿脚本..."
-wget -qO- https://gh-proxy.com/https://raw.githubusercontent.com/ShoufuMining/aleo-windows-f2pool/main/shoufu_f2pool_aleominer.sh | bash
-
-# 提示用户脚本已启动
-echo "Aleo 挖矿脚本已成功启动！"
