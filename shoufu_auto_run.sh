6158c380e111b8c9e6952595a0a66b0532da5cbc
shoufu <huge20901@gmail.com>
2024-10-24 21:32:57 +0800

    Update shoufu_auto_run.sh

--- shoufu_auto_run.sh
+++ shoufu_auto_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-clear  # 清屏
-
 purple="\033[35m"
-reset="\033[0m"  # 用于重置颜色
+green="\033[32m"  # 设置绿色
+reset="\033[0m"   # 用于重置颜色
 
+# 显示引导文本
 echo -e "${purple}---------------------------------"
 echo -e "|  ----首----     ----富----   |"
 echo -e "| |         |   |          |  |"
@@ -21,12 +21,20 @@ echo "加入首富电报财富群: https://t.me/shoufuhuge 一起暴富"
 echo "首富x推特: https://x.com/huge2090"
 echo "========================================================================"
 
+# 提示用户按任意键继续
+echo -e "${green}按任意键继续...${reset}"
+read -n 1
+
+# 清屏
+clear 
+
 # 显示主菜单
 while true; do
     echo "========================================================================" 
     echo "1) 安装 ALEO 一键挖矿脚本" 
-    echo "2) 待更新..." 
+    echo "2) 安装Frp反向代理软件" 
     echo "q) 退出" 
+    echo "========================================================================" 
     read -p "请输入选项: " OPTION 
 
     case $OPTION in 
@@ -38,7 +46,7 @@ while true; do
             ./shoufu_f2pool_aleominer.sh
             ;; 
         2) 
-            echo "待更新..." 
+            echo -e "${purple}待更新...${reset}" 
             ;; 
         q) 
             echo "退出程序。" 
@@ -51,6 +59,6 @@ while true; do
     esac 
 
     # 提示用户按任意键返回主界面
-    echo "按任意键返回主界面..."
+    echo -e "${green}按任意键返回主界面...${reset}"
     read -n 1  # 等待用户按下任意键
-done
+done 
